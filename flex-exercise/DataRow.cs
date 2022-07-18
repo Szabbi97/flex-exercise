289d168a8ff78f9f33b7d15d029fba6db04c5f78
SzabolcsBerenyi <szabolcs.berenyi@atmpoint.eu>
2022-07-18 15:38:49 +0200

    Fully working project

---
+++ flex-exercise/DataRow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flex_exercise
+{
+    public class DataRow
+    {
+        public int MinCount { get; set; }
+        public int MaxCount { get; set; }
+        public char CharacterToContain { get; set; }
+        public string Password { get; set; }
+        public bool IsRightPassword
+        {
+            get
+            {
+                int count = Password.Count(f => f == CharacterToContain);
+                return MinCount <= count && count <= MaxCount;
+            }
+        }
+        public DataRow(string inputRow)
+        {
+            // datarow example: 8-9 b: pbbbbbbkbz
+            string[] inputRowDivided = inputRow.Split(' ');
+            MinCount = int.Parse(inputRowDivided[0].Split('-')[0]);
+            MaxCount = int.Parse(inputRowDivided[0].Split('-')[1]);
+            CharacterToContain = inputRowDivided[1][0];
+            Password = inputRowDivided[2];
+        }
+        public DataRow(int minCount, int maxCount, char characterToContain, string password)
+        {
+            MinCount = minCount;
+            MaxCount = maxCount;
+            CharacterToContain = characterToContain;
+            Password = password;
+        }
+    }
+}
---
+++ flex-exercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flex_exercise
+{
+    internal class Program
+    {
+        static readonly string textFile = "input.txt";
+        static void Main(string[] args)
+        {
+            List<DataRow> DataRowList = new List<DataRow>();
+            if (File.Exists(textFile))
+            {
+                using (StreamReader file = new StreamReader(textFile))
+                {
+                    int counter = 0;
+                    string ln;
+
+                    while ((ln = file.ReadLine()) != null)
+                    {
+                        counter++;
+                        DataRow row = new DataRow(ln);
+                        DataRowList.Add(row);
+                    }
+                    file.Close();
+                }
+                int NumberOfRightPasswords = 0;
+                foreach (DataRow row in DataRowList)
+                {
+                    if (row.IsRightPassword)
+                    {
+                        NumberOfRightPasswords++;
+                    }
+                }
+                Console.WriteLine(NumberOfRightPasswords);
+                Console.ReadKey();
+            }
+        }
+    }
+}
